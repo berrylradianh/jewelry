4e79b2cae824802a98efd5ea80943b8b35fccbb8
berrylradianh <berrylhamesha@gmail.com>
2023-04-08 21:55:54 +0700

    adding content homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,16 @@ module.exports = {
   content: [
     "./src/*.{html,js}",
     "./assets/js/*.{js}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('@tailwindcss/aspect-ratio'),
+    require("tw-elements/dist/plugin")
 ]
 }
 
